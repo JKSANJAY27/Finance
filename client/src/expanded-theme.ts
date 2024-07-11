3917fee647cc779a3dae6dce6b62421fe69994fa
SANJAY J K <j.k.sanjay2006@gmail.com>
2024-07-11 11:58:10 +0530

    commit

---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,12 @@
+//eslint-disable-next-line
+import "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+    interface PaletteColor {
+        [key: number]: string;
+    }
+
+    interface Palette {
+        tertiary: PaletteColor;
+    }
+}
